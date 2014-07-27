d82a9147c58c14d653cb11a999f1ab1c4dfa2293
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2014-07-28 00:07:19 +0200

    Fix git-up dependency inclusion

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.1",
     packages=find_packages(exclude=["tests"]),
     scripts=['PyGitUpAll/gitupall.py'],
-    install_requires=['GitPython==0.3.2.RC1',# 'PyGitUp==v1.1.4',
+    install_requires=['GitPython==0.3.2.RC1', 'git-up==1.1.4',
                       'termcolor==1.1.0'],
 
     # Tests
@@ -20,6 +20,7 @@ setup(
         ]
     },
 
+    dependency_links=['https://github.com/msiemens/PyGitUp/tarball/master#egg=PyGitUp-1.1.4'],
     zip_safe=False,
 
     # Metadata
