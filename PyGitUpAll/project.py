2c469445eadaf49e0486748627914bc48878e667
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2014-07-29 21:10:32 +0200

    Create project attributes while creating new obj

--- PyGitUpAll/project.py
+++ PyGitUpAll/project.py
@@ -4,13 +4,19 @@
 
 class Project(object):
 
-    project_properties = [
+    __allowed_properties = [
         'name',
         'git_url',
         'absolute_path'
     ]
 
     def __new__(cls, project_settings):
-        if not sorted(project_settings.keys()) == sorted(cls.project_properties):
+        # Check if project settings are a go for setting attributes
+        if not sorted(project_settings.keys()) == sorted(cls.__allowed_properties):
             return None
+
+        # Create attributes from provided dict
+        for allowed_property in cls.__allowed_properties:
+            setattr(cls, allowed_property, project_settings[allowed_property])
+
         return cls
