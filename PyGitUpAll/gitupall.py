7b401c55cd05bcb640c4de3ef9a52a431e7f97da
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2014-07-31 23:31:27 +0200

    Add support for projects without a git.url and so on sourcetree

--- PyGitUpAll/gitupall.py
+++ PyGitUpAll/gitupall.py
@@ -5,7 +5,7 @@ import os
 import sys
 import docopt
 
-from git import Repo
+from git import Repo, exc
 from git.repo.fun import is_git_dir
 from PyGitUp.git_wrapper import GitError
 from PyGitUp.gitup import GitUp
@@ -36,29 +36,26 @@ class GitUpAll(object):
 
 
         for project_name, project_settings in projects.iteritems():
+
+            """
+            This nees to be rewritten and tested!
+            """
             # Check if we are able to initialize settings to an object
             project = Project(project_settings)
-            print(colored("- Working on: " + project.name + " @" + project.absolute_path, attrs=["underline"]))
             if not project:
-                print (colored("Could not create project " + project_name, color="red"))
+                print (colored("Could not read project " + project_name, color="red"))
                 continue
+            print(colored("- Working on: " + project.name + " @" + project.absolute_path, attrs=["underline"]))
 
             # Check if a dir exists or clone
-            if not os.path.isdir(project.absolute_path):
+            if (not os.path.isdir(project.absolute_path) or os.listdir(project.absolute_path) == []) and project.git_url:
                 print ("Project is not directory or not initialized repo. Cloning from url")
                 project.repo = Repo.clone_from(url=project.git_url, to_path=project.absolute_path)
                 print (colored("Repository cloned", color="green"))
 
-
-            # Check if dir is empty and if yes clone
-            if os.listdir(project.absolute_path) == []:
-                print ("Project is not initialized repo. Cloning from url")
-                project.repo = Repo.clone_from(url=project.git_url, to_path=project.absolute_path)
-                print (colored("Repository cloned", color="green"))
-
             # Check if the is not git and if not skip
-            if not is_git_dir(os.path.join(self.start_dir, ".git")):
-                print (colored("Project path is not a git dir and contains files. Skipping", color="red"))
+            if not is_git_dir(os.path.join(project.absolute_path, ".git")) and not project.git_url:
+                print (colored("Project path is not a git dir and has no info. Skipping", color="red"))
                 continue
 
             self.sync_repository(project)
@@ -74,7 +71,7 @@ class GitUpAll(object):
         try:
             # Creates a new object
             GitUp().run()
-        except GitError:
+        except (GitError, exc.GitCommandError) as e:
             print (colored("Could not update repository: " + project.name, color="red"))
         finally:
             os.chdir(cls.start_dir)
